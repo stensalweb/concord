b33041af3ec129caf6c6a5c9daffd4d092fb7815
LucasMull <muller.lucas@Hotmail.com>
2020-11-12 18:23:38 -0300

    feat: add extra debug options to make debugging messages prettier

--- src/debug.h
+++ src/debug.h
@@ -10,10 +10,13 @@
 #       define DEBUG_FMT_ARGS __FILE__, __LINE__, __func__
 /* @param msg string to be printed in debug mode */
 #       define DEBUG_PUTS(msg) fprintf(DEBUG_OUT, DEBUG_FMT_PREFIX "%s\n", DEBUG_FMT_ARGS, msg)
+#       define DEBUG_NOTOP_PUTS(msg) fprintf(DEBUG_OUT, "\t%s\n", msg)
 /* @param fmt like printf
    @param ... arguments to be parsed into fmt */
-#       define __DEBUG_PRINT_HELPER(fmt, ...) fprintf(DEBUG_OUT, DEBUG_FMT_PREFIX fmt "\n%s", DEBUG_FMT_ARGS, __VA_ARGS__)
-#       define DEBUG_PRINT(...) __DEBUG_PRINT_HELPER(__VA_ARGS__, "")
+#       define __DEBUG_PRINT(fmt, ...) fprintf(DEBUG_OUT, DEBUG_FMT_PREFIX fmt"\n%s", DEBUG_FMT_ARGS, __VA_ARGS__)
+#       define DEBUG_PRINT(...) __DEBUG_PRINT(__VA_ARGS__, "")
+#       define __DEBUG_NOTOP_PRINT(fmt, ...) fprintf(DEBUG_OUT, "\t"fmt"\n%s", __VA_ARGS__)
+#       define DEBUG_NOTOP_PRINT(...) __DEBUG_NOTOP_PRINT(__VA_ARGS__, "")
 /* @param expr to be checked for its validity
    @param msg to be printed in case of invalidity */
 #       define DEBUG_ASSERT(expr, msg) \
@@ -28,7 +31,9 @@
 #       define DEBUG_ONLY(arg) (arg)
 #else /* DEBUG MODE INNACTIVE */
 #       define DEBUG_PUTS(msg) 
+#       define DEBUG_NOTOP_PUTS(msg) 
 #       define DEBUG_PRINT(...)
+#       define DEBUG_NOTOP_PRINT(...)
 /* DEBUG_ASSERT becomes a proxy for assert */
 #       include <assert.h>
 #       define DEBUG_ASSERT(expr, msg) assert(expr)
